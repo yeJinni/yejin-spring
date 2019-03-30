04ad3c03cd9edd4fdbc170b5bff69ba7ed210c96
yeJinni <skyrocket0240@naver.com>
2019-03-30 23:20:07 +0900

    yejin-spring

---
+++ src/main/java/org/yejin/book/chap03/MemberMain.java
@@ -0,0 +1,18 @@
+package org.yejin.book.chap03;
+
+public class MemberMain {
+
+	public static void main(String[] args) {
+		MemberDao memberDao = new MemberDao();
+		MemberRegisterService regService = new MemberRegisterService(memberDao);
+
+		// registerRequest 초기화
+		RegisterRequest req = new RegisterRequest();
+		req.setEmail("skyrocket0240@naver.com");
+		req.setPassword("yj102030!^^");
+		req.setName("yeJinni");
+
+		// 회원 등록
+		regService.regist(req);
+	}
+}
