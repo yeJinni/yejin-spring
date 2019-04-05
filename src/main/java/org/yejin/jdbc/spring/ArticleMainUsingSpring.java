ae9dcd3ba767b3155c113f8bfcba61ad1533fb44
yejin <skyrocket0240@naver.com>
2019-04-05 11:32:34 +0900

    yejin-spring

---
+++ src/main/java/org/yejin/jdbc/spring/ArticleMainUsingSpring.java
@@ -0,0 +1,24 @@
+package org.yejin.jdbc.spring;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ArticleService를 테스트하는 main
+ * 
+ * @author Jacob
+ */
+public class ArticleMainUsingSpring {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring-jdbc.xml");
+		ArticleService articleService = context.getBean("articleService",
+				ArticleService.class);
+		context.close();
+
+		articleService.addArticle();
+		articleService.listArticles();
+	}
+
+}
