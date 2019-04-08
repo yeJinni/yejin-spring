ccee6b716f94e587e10e430b98431bb9b9df11e6
yejin <skyrocket0240@naver.com>
2019-04-08 13:46:31 +0900

    yejin-spring

--- src/main/java/org/yejin/book/chap07/MemberService.java
+++ src/main/java/org/yejin/book/chap07/MemberService.java
@@ -1,5 +1,50 @@
 package org.yejin.book.chap07;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.yejin.book.chap03.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * p.204 [리스트 8.13]를 Main과 Service로 분리한 Service 부분
+ * 
+ * @author Jacob
+ */
+@Service
 public class MemberService {
 
+	Logger logger = LogManager.getLogger();
+	@Autowired
+	MemberDao memberDao;
+
+	/**
+	 * memberDao setter for injection
+	 */
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	public void selectAll() {
+		List<Member> members = memberDao.selectAll(0, 100);
+		logger.debug(members);
+	}
+
+	public void updateMember() {
+		Member member = memberDao.selectByEmail("skyrocket0240@naver.com");
+		member.setPassword("yj102030!^^");
+		memberDao.update(member);
+		logger.debug("Update complete.");
+	}
+
+	public void insertMember() {
+		Member member = new Member();
+		member.setEmail("parkyj220@naver.com");
+		member.setPassword("yj102030!^^");
+		member.setName("박예진진");
+		memberDao.insert(member);
+		logger.debug("Insert complete.");
+	}
 }
